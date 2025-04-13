319d2377f641df89c3a96ddee68698be5efbb2f4
Moritz Kopmann <moritz-oskar@web.de>
2025-04-13 12:17:00 +0200

    expose model

--- lib/mvu.dart
+++ lib/mvu.dart
@@ -1,3 +1,4 @@
 export 'src/event.dart' show Event, OutEvent;
 export 'src/model_provider.dart' show ModelProvider;
+export 'src/model.dart' show Model;
 export 'src/state_view.dart' show StateView;
